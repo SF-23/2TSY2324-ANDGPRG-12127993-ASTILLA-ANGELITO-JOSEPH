845d1e837cdfbec0902a1e006652d3ef34a671cc
Astilla Angelito Joseph <angelitojoseph.astilla@benilde.edu.ph>
2024-01-18 13:03:56 +0800

    Revert "Unity Quiz 1 Updated"
    This reverts commit 720b3ca4538156777d873296c9f78d644b6cdf2d.

--- SolarSystemExplore/Assets/Scripts/Player.cs
+++ SolarSystemExplore/Assets/Scripts/Player.cs
@@ -7,11 +7,41 @@ public class Player : MonoBehaviour
 {
 	[SerializeField] float moveSpeed = 5f;
 	[SerializeField] Rigidbody rb;
-	[SerializeField] float rotateSpeed = 30f;
 
+    //called before start
+	private void Awake()
+	{
+	
+	}
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+
+	private void FixedUpdate()
+	{
+		
+	}
 	// Update is called once per frame
 	void Update()
     {
+		//this.transform.position += new Vector3(0, 0, 1);
+
+		/*if(Input.GetKeyDown(KeyCode.W))
+		{
+			// on press but only once
+		}
+		if (Input.GetKey(KeyCode.W))
+		{
+			// while pressing the key
+			//this.transform.position += new Vector3(0, 0, 1);
+		}
+		if(Input.GetKeyUp(KeyCode.W))
+		{
+			// on release but only once
+		}*/
 		MovementControl();
 	}
 
@@ -19,11 +49,11 @@ public class Player : MonoBehaviour
 	{
 		if (Input.GetKey(KeyCode.W))
 		{
-			this.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+			this.transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
 		}
 		if (Input.GetKey(KeyCode.S))
 		{
-			this.transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+			this.transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
 		}
 		if (Input.GetKey(KeyCode.A))
 		{
@@ -31,24 +61,12 @@ public class Player : MonoBehaviour
 		}
 		if (Input.GetKey(KeyCode.D))
 		{
+			//this.rb.velocity += Vector3.right;
 			this.transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
 		}
-        if (Input.GetKey(KeyCode.Q))
-        {
-            this.transform.Rotate(Vector3.up, -rotateSpeed * Time.deltaTime);
-        }
-		if (Input.GetKey(KeyCode.E))
-		{
-            this.transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.Space)) 
-		{
-			this.transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
-		}
-		if(Input.GetKey(KeyCode.LeftControl)) 
-		{
-			this.transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
-		}
+	}
+	private void LateUpdate()
+	{
 		
 	}
 
@@ -61,8 +79,8 @@ public class Player : MonoBehaviour
 		{
 			Debug.Log("Im bumping to This object: " + other.gameObject.name);
 		}
-
-    }
+		
+	}
 
 	private void OnCollisionEnter(Collision other)
 	{
